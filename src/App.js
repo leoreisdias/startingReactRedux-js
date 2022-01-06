56052595fc04e8f33bc5f21b92aaeb8156b4bf0c
leoreisdias <leonardord99@gmail.com>
2022-01-06 18:41:55 -0300

    feat: async dispatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { fetchToken } from "./store/login";
 import { add } from "./store/reducers";
 
 function App() {
-  const state = useSelector(state => state.total);
+  const {reducer} = useSelector(state => state);
   const dispatch = useDispatch()
+  
+  useEffect(() => {
+    dispatch(fetchToken({username: 'dog', password: 'dog'}));
+
+  },[dispatch])
+
   return (
     <div className="App">
-      Total: {state}
+      Total: {reducer.total}
 
       <button onClick={() => dispatch(add())}>Add on Total</button>
     </div>
