9ba7099fbb4891b17aeba310a2c466d04b7c608b
leoreisdias <leonardord99@gmail.com>
2022-01-06 17:03:02 -0300

    feat: Hooks and states -> useDispatch and useSelector

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function App() {
+  const state = useSelector(state => state);
+  console.log(state);
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Total: {state}
+
+      <button onClick={() => dispatch({type: 'ADD'})}>Add on Total</button>
     </div>
   );
 }
